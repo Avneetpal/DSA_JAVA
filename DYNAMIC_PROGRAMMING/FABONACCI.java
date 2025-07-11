4be287ec66e7701338a33de893c9dd5532aedcb3
AVNEET PAL <127130851+Avneetpal@users.noreply.github.com>
2025-07-11 12:18:17 +0530

    Update FABONACCI.java

--- DYNAMIC_PROGRAMMING/FABONACCI.java
+++ DYNAMIC_PROGRAMMING/FABONACCI.java
@@ -14,19 +14,15 @@ class calculte
         f[n]=fab(n-1, f)+fab(n-2, f);
         return f[n];
     }
-    public static int fib_tab(int n , int[] f)
+    public static int fib_tab(int n )
     {
-        if(n==0 || n==1)
-        {
-            return n;
-        }
-        if(f[n]!=0)
-        {
-            return f[n];
-        }
+        int [] f=new int[n+1];
+        f[0]=0;
+        f[1]=1;
+        
         for(int i=2;i<=n;i++)
         {
-            f[n]=f[n-1]+f[n-2];
+            f[i]=f[i-1]+f[i-2];
         }
         return f[n];
     }
@@ -38,7 +34,7 @@ public class FABONACCI {
         int n=sc.nextInt();
         int[] f=new int[n+1];// empty array
         System.out.println(calculte.fab(n, f));
-        System.out.println(calculte.fib_tab(n,f));
+        System.out.println(calculte.fib_tab(n));
         sc.close();
     }
 }
