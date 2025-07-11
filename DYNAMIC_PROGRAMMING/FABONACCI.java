4cecc7db04697ea4c036572d918fbf34a41c55ea
AVNEET PAL <127130851+Avneetpal@users.noreply.github.com>
2025-07-11 12:10:14 +0530

    Add files via upload

--- DYNAMIC_PROGRAMMING/FABONACCI.java
+++ DYNAMIC_PROGRAMMING/FABONACCI.java
@@ -14,6 +14,22 @@ class calculte
         f[n]=fab(n-1, f)+fab(n-2, f);
         return f[n];
     }
+    public static int fib_tab(int n , int[] f)
+    {
+        if(n==0 || n==1)
+        {
+            return n;
+        }
+        if(f[n]!=0)
+        {
+            return f[n];
+        }
+        for(int i=2;i<=n;i++)
+        {
+            f[n]=f[n-1]+f[n-2];
+        }
+        return f[n];
+    }
 }
 public class FABONACCI {
 
@@ -22,6 +38,7 @@ public class FABONACCI {
         int n=sc.nextInt();
         int[] f=new int[n+1];// empty array
         System.out.println(calculte.fab(n, f));
+        System.out.println(calculte.fib_tab(n,f));
         sc.close();
     }
 }
